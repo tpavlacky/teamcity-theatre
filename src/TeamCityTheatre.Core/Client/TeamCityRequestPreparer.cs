039068344d438bfec70d601466f985bf16408001
Alexander Moerman <amoerman@csc.com>
2018-05-11 10:07:17 +0200

    Add logging teamcity client

--- src/TeamCityTheatre.Core/Client/TeamCityRequestPreparer.cs
+++ src/TeamCityTheatre.Core/Client/TeamCityRequestPreparer.cs
@@ -2,6 +2,11 @@
 
 namespace TeamCityTheatre.Core.Client {
   public class TeamCityRequestPreparer : ITeamCityRequestPreparer {
+
+    public TeamCityRequestPreparer() {
+      
+    }
+
     public void Prepare(IRestRequest request) {
       request.DateFormat = "yyyyMMdd'T'HHmmsszzz";
     }
