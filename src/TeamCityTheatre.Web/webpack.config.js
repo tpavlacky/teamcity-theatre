b865cfaca7539eeacfe389d22aa8cdac8452ae43
Alexander Moerman <amoerman@csc.com>
2018-05-14 14:03:22 +0200

    Upgrade to Webpack 4

---
+++ src/TeamCityTheatre.Web/webpack.config.js
@@ -0,0 +1,34 @@
+﻿const path = require("path");
+
+module.exports = function () {
+  return {
+    stats: {
+      hash: false,
+      version: false,
+      assets: true
+    },
+    entry: {
+      "dashboard": "./views/home/dashboard.js",
+      "settings": "./views/home/settings.js"
+    },
+    output: {
+      path: path.resolve(__dirname, "wwwroot/js"),
+      filename: "[name].js"
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendor",
+            chunks: "all"
+          }
+        }
+      }
+    },
+    performance: {
+      maxEntrypointSize: 524288, // 512 kb
+      maxAssetSize: 524288 // // 512 kb
+    }
+  };
+};
