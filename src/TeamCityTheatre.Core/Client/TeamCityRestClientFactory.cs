86b0c0adfa757228b64ac3f01e3d1a5291537766
Alexander Moerman <a.moerman@dobcomed.com>
2019-01-10 22:40:27 +0100

    Update to newest RestSharp and use Newtonsoft.Json to deserialize, which solves datetime conversion problems

--- src/TeamCityTheatre.Core/Client/TeamCityRestClientFactory.cs
+++ src/TeamCityTheatre.Core/Client/TeamCityRestClientFactory.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using RestSharp;
 using RestSharp.Authenticators;
+using RestSharp.Serialization;
 using TeamCityTheatre.Core.Options;
 
 namespace TeamCityTheatre.Core.Client {
@@ -11,13 +14,40 @@ namespace TeamCityTheatre.Core.Client {
   public class TeamCityRestClientFactory : ITeamCityRestClientFactory {
     public IRestClient Create(ConnectionOptions connectionOptions) {
       if (connectionOptions == null) throw new ArgumentNullException(nameof(connectionOptions));
-      return new RestClient {
+      var client = new RestClient {
         BaseUrl = new Uri(new Uri(connectionOptions.Url), new Uri("httpAuth/app/rest", UriKind.Relative)),
         Authenticator = new HttpBasicAuthenticator(connectionOptions.Username, connectionOptions.Password),
-        DefaultParameters = {
-          new Parameter {Type = ParameterType.HttpHeader, Name = "Accept", Value = "application/json"}
+      }.UseSerializer(new JsonNetSerializer());
+
+      client.DefaultParameters.Add(new Parameter {Type = ParameterType.HttpHeader, Name = "Accept", Value = "application/json"});
+      
+      return client;
+    }
+  }
+
+  public class JsonNetSerializer : IRestSerializer {
+    readonly JsonSerializerSettings _jsonSerializerSettings;
+
+    public JsonNetSerializer() {
+      _jsonSerializerSettings = new JsonSerializerSettings {
+        Converters = {
+          new IsoDateTimeConverter { DateTimeFormat = "yyyyMMdd'T'HHmmsszzz" }
         }
       };
     }
+    
+    public string Serialize(object obj) => JsonConvert.SerializeObject(obj, _jsonSerializerSettings);
+
+    public T Deserialize<T>(IRestResponse response) => JsonConvert.DeserializeObject<T>(response.Content, _jsonSerializerSettings);
+
+    public string Serialize(Parameter parameter) => JsonConvert.SerializeObject(parameter.Value, _jsonSerializerSettings);
+
+    public string[] SupportedContentTypes { get; } = {
+      "application/json", "text/json", "text/x-json", "text/javascript", "*+json"
+    };
+
+    public string ContentType { get; set; } = "application/json";
+
+    public DataFormat DataFormat { get; } = DataFormat.Json;
   }
 }
